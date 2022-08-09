3bf2698f8939a8bf2ca1944a1c509a3aee9782da
Victor <victorroldan.dev@gmail.com>
2022-08-09 12:33:58 -0500

    first commit

--- SearchableTutorial/ContentView.swift
+++ SearchableTutorial/ContentView.swift
@@ -7,14 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct CountriesModel : Codable{
+    let name : String
+    let code : String
+    
+    static func loadJson() -> [CountriesModel]?{
+        guard let url = Bundle.main.url(forResource: "Countries", withExtension: "json") else{ return nil }
+        do{
+            let data = try Data(contentsOf: url)
+            let countries = try JSONDecoder().decode([CountriesModel].self, from: data)
+            return countries
+        }catch{
+            return nil
         }
+    }
+}
+
+class ViewModel : ObservableObject{
+    @Published var countries = [CountriesModel]()
+    
+    init(){
+        countries = CountriesModel.loadJson() ?? []
+    }
+}
+
+private func flag(country:String) -> String {
+    let base : UInt32 = 127397
+    var s = ""
+    for v in country.unicodeScalars {
+        s.unicodeScalars.append(UnicodeScalar(base + v.value)!)
+    }
+    return String(s)
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var searchText : String = ""
+    
+    var results : [CountriesModel]{
+        if searchText.isEmpty{
+            return viewModel.countries
+        }else{
+            return viewModel.countries.filter{$0.name.uppercased().contains(searchText.uppercased())}
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List{
+                    ForEach(results, id: \.code) { countries in
+                        HStack {
+                            Text(flag(country:countries.code))
+                                .font(.system(size: 30))
+                            Text(countries.name)
+                        }
+                    }
+                }.listStyle(.plain)
+            }
+            .navigationTitle("Paises")
+        }
+        .searchable(
+            text: $searchText,
+            placement: .navigationBarDrawer(displayMode: .automatic),
+            prompt: Text("Escribe el nombre de un pais"))
     }
 }
 
