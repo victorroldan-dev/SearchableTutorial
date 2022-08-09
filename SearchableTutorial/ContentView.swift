30ff20e9b8a20619da9d38a7ef76df35a4f9465b
Victor <victorroldan.dev@gmail.com>
2022-08-08 19:07:44 -0500

    Initial Commit

---
+++ SearchableTutorial/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SearchableTutorial
+//
+//  Created by Victor Roldan on 8/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchableTutorial/SearchableTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableTutorialApp.swift
+//  SearchableTutorial
+//
+//  Created by Victor Roldan on 8/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchableTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
